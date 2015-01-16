8f85bcf65b594d2649b88d61707e5147e3d52bb7
India Dearlove <india_dearlove@yahoo.co.uk>
2015-01-16 13:09:07 +0000

    not getting Deprecation Warnings anymore

--- spec/signing_up_spec.rb
+++ spec/signing_up_spec.rb
@@ -4,7 +4,7 @@ require './app/server'
 context "User signs up" do
 
   it "should be able to sign up" do
-    expect(page).to have_content("Welcome to Chitter!")
+    expect(link).to have_content("Welcome to Chitter!")
     fill_in :email, :with => "test@test.com"
     fill_in :password, :with => "1234"
     fill_in :password_confirmation, :with => "1234"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,10 +6,6 @@ require 'capybara/rspec'
 Capybara.app = Sinatra::Application
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.order = 'random'
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
